d7c12bce5b569952dd5e6c181d589d40445193f7
takkaria <takkaria@3ca42a8f-7b31-0410-a887-e817dd3e653f>
2002-10-25 15:02:13 +0000

    [Patch #13] This patch addresses the issues caused by dynamically sizing the town to screen size, in particular crashes on generation.

--- unangband/src/xtra2.c
+++ unangband/src/xtra2.c
@@ -2765,12 +2765,12 @@ bool modify_panel(int wy, int wx)
 	get_zone(&zone,p_ptr->dungeon,p_ptr->depth);
 
 	/* Verify wy, adjust if needed */
-	if (!zone->fill) wy = SCREEN_HGT;
+	if (!zone->fill) wy = 0;
 	else if (wy > DUNGEON_HGT - SCREEN_HGT) wy = DUNGEON_HGT - SCREEN_HGT;
 	else if (wy < 0) wy = 0;
 
 	/* Verify wx, adjust if needed */
-	if (!zone->fill) wx = SCREEN_WID;
+	if (!zone->fill) wx = 0;
 	else if (wx > DUNGEON_WID - SCREEN_WID) wx = DUNGEON_WID - SCREEN_WID;
 	else if (wx < 0) wx = 0;
 
