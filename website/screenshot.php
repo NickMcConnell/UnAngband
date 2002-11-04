502f5e4500fbd18f703e2d25191be1e93bb51d8f
takkaria <takkaria@3ca42a8f-7b31-0410-a887-e817dd3e653f>
2002-11-04 19:07:02 +0000

    Finish the screenshot code.

--- website/screenshot.php
+++ website/screenshot.php
@@ -1,2 +1,37 @@
-<?
+<?php
+
+function put_image($file, $alt_text, $extras='border="0"')
+{
+   $size = getimagesize($file);
+
+   echo '<img width="'.$size[0].'" height="'.$size[1].'" alt="'.$alt_text.'" src="'.$file.'" '.$extras.'>';
+}
+
+$file = 'screenshots/' . $shot . '.gif';
+$desc = 'screenshots/' . $shot . '.desc';
+
+if ((isset($shot)) && (file_exists($file)) && (file_exists($desc)))
+{
+	// Header the page
+	$title = 'Screenshots: '.ucfirst($shot);
+	include('layout/header');
+
+	// Include the image
+	echo '<p align="center">';
+	put_image($file, '');
+	echo '</p>';
+
+	// Include the text
+	echo '<p align="center">';
+	include($desc);
+	echo '</p>';
+}
+else
+{
+	$title = 'Screenshot not found';
+	include('layout/header');
+}
 ?>
+
+
+<?php include('layout/footer'); ?>
