25688b68ec17a5b9813d88a4d7b26adeb8189746
takkaria <takkaria@3ca42a8f-7b31-0410-a887-e817dd3e653f>
2002-11-04 18:46:41 +0000

    Add the new PHP-driven site files.

---
+++ website/index.php
@@ -0,0 +1,25 @@
+<?php
+
+$title = 'The Unnamed Angband'
+include('layout/header');
+
+?>
+			<h2>Welcome</h2>
+			<p>Welcome to Unangband, my variant of Angband. Angband has a long history of development: I recommend you check out the excellent resources of <a href="http://www.thangorodrim.net/">www.thangorodrim.net</a> and <a href="http://angband.oook.cz/">angband.oook.cz</a> if you are interested in the history and current state of the play for this game.</p>
+			<p>This page is here primarily as a place-holder: I'm going to be in Europe on a working holiday until the end of 2003, so it's best to contact me in the forums at this website for all Unangband related questions. Andrew Sidwell is acting as a caretaker during this period; he is developing his own Angband variant, <a href="http://nangband.berlios.de/">Nangband</a>, so his primary focus is elsewhere. Just remember that if you start bugging either of us for support, we probably don't have the time or energy to help you at this stage.</p>
+			<p>Having said that, I hope I haven't put you off. I highly recommend you download the latest version of Unangband and start playing. Don't forget to upload your character dumps to the Angband Ladder at angband.oook.cz and get bragging...</p>
+
+			<h2>Other Resources</h2>
+			<ul>
+				<li>The Berlios.de project page is at <a href="http://developer.berlios.de/projects/unangband/">http://developer.berlios.de/projects/unangband/</a>, which contains links to various downloads.</li>
+				<li>You can browse a web facing copy of the CVS at <a href="http://cvs.berlios.de/cgi-bin/viewcvs.cgi/unangband/">http://cvs.berlios.de/cgi-bin/viewcvs.cgi/unangband/</a>.</li>
+				<li>The Yahoo Groups <a href="http://groups.yahoo.com/group/unangband">mailing list</a> - this is the primary point of discussion for Unangband.<br>
+				<li>I have created another mailing list hosted by Berlios.de, which is intended for <a href="http://developer.berlios.de/mail/?group_id=331">developer related discussion</a>.</li>
+				<li>I have a personal website at <a href="http://promisedpoems.editthispage.com/">promisedpoems.editthispage.com</a> that featured some Unangband related material, if you are after some more.</li>
+			</ul>
+
+<?
+
+include('layout/footer');
+
+?>
