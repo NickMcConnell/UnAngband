1b878165ca02c8227d858bb4a6601ae833718cc6
mikon <mikon@3ca42a8f-7b31-0410-a887-e817dd3e653f>
2006-12-19 22:11:48 +0000

    * I've made the equipment menu (as opposed to the inventory menu) default for 'f'iring.

--- unangband/src/object1.c
+++ unangband/src/object1.c
@@ -3658,6 +3658,13 @@ bool get_item(int *cp, cptr pmt, cptr str, int mode)
 			p_ptr->command_wrk = (USE_EQUIP);
 		}
 
+		/* Hack -- Start on equipment if for shooting */
+		if ((p_ptr->command_cmd == 'f')
+			&& allow_equip)
+		{
+			p_ptr->command_wrk = (USE_EQUIP);
+		}
+
 		/* Use inventory if allowed */
 		else if (use_inven)
 		{
