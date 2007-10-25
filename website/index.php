c1d7b4704e73f6f13e44af61950d7f67aad2e4d9
andrewdoull <andrewdoull@3ca42a8f-7b31-0410-a887-e817dd3e653f>
2007-10-25 07:39:27 +0000

    - Add website redirect to new home page.

--- website/index.php
+++ website/index.php
@@ -1,28 +1,16 @@
 <?php
 
-$title = 'The Unnamed Angband';
-include('layout/header');
+$title = 'The Unnamed Angband - main website has moved. ';
 
 ?>
-      <h2>Welcome</h2>
 
-      <p>Welcome to Unangband, my variant of Angband. Angband has a
-long history of development: I recommend you check out the excellent
-resources at <a href="http://angband.oook.cz/">angband.oook.cz</a> and the 
-<a href="http://en.wikipedia.org/wiki/Angband_%28computer_game%29">wikipedia entry on Angband</a> if you are interested in
- the history and current state of the play for this game.</p>
-
-      <p>I highly recommend you download the <a href="http://developer.berlios.de/project/showfiles.php?group_id=331">latest version</a>
-of Unangband and start playing. Don't forget to upload your character
-dumps to the Angband Ladder at angband.oook.cz and get bragging...</p>
-
-
-	<p>If you are interested in the development side of Angband, check out the links under the resources section to the left,
-	including my development blog, which is updated regularly. The only thing updated more frequently is the SVN source code
-	repository, and you can browse that for tid bits on what will be in the next Unangband release. Of particular interest to
-	you will be the change log and todo list that are maintained in the repository.</p>
-
+<html><head>
+  <meta http-equiv="Refresh" content="5; url=http://unangband.blogspot.com/">
+</head><body>
+  Please update your links to point to <a href="http://unangband.blogspot.com/">unangband.blogspot.com</a>.
 <br>
+  You will be redirected in 5 seconds.
+</body></html>
 
 
 <?php include('layout/footer'); ?>
