34412acb55ec90cb6c9da2da0ad4356bd56b19e3
andrewdoull <andrewdoull@3ca42a8f-7b31-0410-a887-e817dd3e653f>
2007-11-20 06:34:56 +0000

    - Lower damage bonus for 'Deadly Shots' spell and remove level modifier, as this otherwise makes this spell too effective.

--- unangband/src/spells2.c
+++ unangband/src/spells2.c
@@ -7999,13 +7999,13 @@ bool process_spell_types(int who, int spell, int level, bool *cancel)
 				bool result;
 				
 				/* Accurate blow */
-				p_ptr->to_h = s_ptr->param + p_ptr->lev;
+				p_ptr->to_h = s_ptr->param;
 				
 				/* Allow direction to be cancelled for free */
 				result = process_spell_blow_shot_hurl(s_ptr->type - SPELL_ACCURATE_BLOW);
 
 				/* End branding */
-				p_ptr->to_h -= s_ptr->param + p_ptr->lev;
+				p_ptr->to_h -= s_ptr->param;
 
 				if (!result) return (!(*cancel));
 
@@ -8021,13 +8021,13 @@ bool process_spell_types(int who, int spell, int level, bool *cancel)
 				bool result;
 				
 				/* Damaging blow */
-				p_ptr->to_h += s_ptr->param + p_ptr->lev;
+				p_ptr->to_h += s_ptr->param;
 				
 				/* Allow direction to be cancelled for free */
 				result = process_spell_blow_shot_hurl(s_ptr->type - SPELL_DAMAGING_BLOW);
 
 				/* End branding */
-				p_ptr->to_h -= s_ptr->param + p_ptr->lev;
+				p_ptr->to_h -= s_ptr->param;
 
 				if (!result) return (!(*cancel));
 
