8993cbad25f897ea6b701d83a1e6b8a994c1484e
mikon <mikon@3ca42a8f-7b31-0410-a887-e817dd3e653f>
2008-05-26 15:26:13 +0000

    * gold: a partial fix to #8412

--- unangband/unangband/src/melee2.c
+++ unangband/unangband/src/melee2.c
@@ -121,20 +121,24 @@ void find_range(int m_idx)
 	/* Maximum range to flee to (reduced elsewhere for themed levels */
 	if (!(m_ptr->min_range < FLEE_RANGE)) m_ptr->min_range = FLEE_RANGE;
 
-	/* Nearby monsters that cannot run away will not become run unless
-	 * completely afraid */
+	/* Nearby monsters that cannot run away, won't unless completely afraid */
 	else if ((m_ptr->cdis < TURN_RANGE) && (m_ptr->mspeed < p_ptr->pspeed))
 		m_ptr->min_range = 1;
 
 	/* Now find prefered range */
 	m_ptr->best_range = m_ptr->min_range;
 
-	/*Note below: Monsters who have had dangerous attacks happen to them are more extreme
-	 * as are monsters that are currenty hidden. */
+	/* Note below: Monsters who have had dangerous attacks happen to them 
+	   are more extreme as are monsters that are currenty hidden. */
 
-	/* Spellcasters want to sit back */
-	if ((r_ptr->freq_spell > ((m_ptr->mflag & (MFLAG_AGGR | MFLAG_HIDE)) != 0 ? 0 : 24)) &&
-			(((r_ptr->flags5 & (RF5_ATTACK_MASK)) != 0) ||
+	/* TODO: take range of spell and innate attacks into account;
+	   update whenever mana levels change, not only when player attacks */
+
+	/* Spellcasters with mana want to sit back */
+	if (r_ptr->freq_spell 
+		> ((m_ptr->mflag & (MFLAG_AGGR | MFLAG_HIDE)) != 0 ? 0 : 24)
+		&& m_ptr->mana >= r_ptr->mana / 6
+		&& (((r_ptr->flags5 & (RF5_ATTACK_MASK)) != 0) ||
 			((r_ptr->flags6 & (RF6_ATTACK_MASK)) != 0) ||
 			((r_ptr->flags7 & (RF7_ATTACK_MASK)) != 0)))
 	{
@@ -149,16 +153,18 @@ void find_range(int m_idx)
 		m_ptr->best_range = 6;
 	}
 	
-	/* Archers want to sit back */
-	else if (((r_ptr->flags2 & (RF2_ARCHER)) != 0) && (find_monster_ammo(m_idx, -1, FALSE) >= 0))
+	/* Archers with ammo want to sit back */
+	else if (((r_ptr->flags2 & (RF2_ARCHER)) != 0) 
+			 && find_monster_ammo(m_idx, -1, FALSE) >= 0)
 	{
 		/* Don't back off for aggression due to limited range */
 		m_ptr->best_range = 6;
 	}
 	
-	/* Innate magic users want to sit back */
-	else if ((r_ptr->freq_innate > ((m_ptr->mflag & (MFLAG_AGGR | MFLAG_HIDE)) != 0 ? 0 : 24)) &&
-			((r_ptr->flags4 & (RF4_ATTACK_MASK)) == 0))
+	/* Innate magic users with mana (or with 0 max mana) want to sit back */
+	else if (r_ptr->freq_innate > ((m_ptr->mflag & (MFLAG_AGGR | MFLAG_HIDE)) != 0 ? 0 : 24)
+			 && m_ptr->mana >= r_ptr->mana / 6
+			 && (r_ptr->flags4 & (RF4_ATTACK_MASK)) != 0)
 	{
 		m_ptr->best_range = 6 + ((m_ptr->mflag & (MFLAG_AGGR)) != 0 ? 2 : 0);
 	}
