2ad736f245d16d0fee21487c143ab10a4c54174c
andrewdoull <andrewdoull@3ca42a8f-7b31-0410-a887-e817dd3e653f>
2009-06-10 14:54:27 +0000

    - Fix for discounts not reducing cost of items.

--- unangband/src/object2.c
+++ unangband/src/object2.c
@@ -1379,102 +1379,98 @@ s32b object_value(const object_type *o_ptr)
 
 		/* Real value (see above) */
 		value = object_value_real(o_ptr);
-
-		return (value);
 	}
 
-	/* Unknown items -- acquire a base value */
-	else
+	/* Hack -- Felt broken items */
+	else if (o_ptr->feeling == INSCRIP_TERRIBLE
+			  || o_ptr->feeling == INSCRIP_WORTHLESS
+			  || o_ptr->feeling == INSCRIP_CURSED
+			  || o_ptr->feeling == INSCRIP_BROKEN)
 	{
-		/* Hack -- Felt broken items */
-		if (o_ptr->feeling == INSCRIP_TERRIBLE
-				  || o_ptr->feeling == INSCRIP_WORTHLESS
-				  || o_ptr->feeling == INSCRIP_CURSED
-				  || o_ptr->feeling == INSCRIP_BROKEN)
-			return (0L);
+		return (0L);
+	}
 
-		/* Named value (use 'real' value and attempt to hack) */
-		if (object_named_p(o_ptr))
+	/* Named value (use 'real' value and attempt to hack) */
+	else if (object_named_p(o_ptr))
+	{
+		object_type object_type_body;
+		object_type *j_ptr = &object_type_body;
+
+		/* Copy object */
+		object_copy(j_ptr, o_ptr);
+
+		/* Remove unknown information */
+		if (!object_bonus_p(o_ptr))
 		{
-			object_type object_type_body;
-			object_type *j_ptr = &object_type_body;
-
-			/* Copy object */
-			object_copy(j_ptr, o_ptr);
-
-			/* Remove unknown information */
-			if (!object_bonus_p(o_ptr))
-			{
-				if (o_ptr->name1)
-				{
-					j_ptr->to_h = a_info[o_ptr->name1].to_h;
-					j_ptr->to_d = a_info[o_ptr->name2].to_d;
-					j_ptr->to_a = a_info[o_ptr->name3].to_a;
-				}
-				else if (o_ptr->name2)
-				{
-					j_ptr->to_h = e_info[o_ptr->name2].max_to_h / 2;
-					j_ptr->to_d = e_info[o_ptr->name2].max_to_d / 2;
-					j_ptr->to_a = e_info[o_ptr->name2].max_to_a / 2;
-				}
-				else
-				{
-					j_ptr->to_h = k_info[o_ptr->to_h].to_h;
-					j_ptr->to_d = k_info[o_ptr->to_d].to_d;
-					j_ptr->to_a = k_info[o_ptr->to_a].to_a;
-				}
-			}
-			if (!object_charges_p(o_ptr)) j_ptr->charges = 0;
-			if (!object_pval_p(o_ptr))
-			{
-				if (o_ptr->name1)
-				{
-					j_ptr->pval = a_info[o_ptr->name1].pval;
-				}
-				else if (o_ptr->name2)
-				{
-					j_ptr->pval = e_info[o_ptr->name2].max_pval / 2;
-				}
-				else
-				{
-					j_ptr->pval = k_info[o_ptr->to_h].pval;
-				}
-			}
-
-			/* Try curse analysis */
 			if (o_ptr->name1)
 			{
-				if (a_info[o_ptr->name1].flags3 & (TR3_LIGHT_CURSE | TR3_HEAVY_CURSE | TR3_PERMA_CURSE | TR3_UNCONTROLLED))
-					j_ptr->ident |= (IDENT_CURSED);
+				j_ptr->to_h = a_info[o_ptr->name1].to_h;
+				j_ptr->to_d = a_info[o_ptr->name2].to_d;
+				j_ptr->to_a = a_info[o_ptr->name3].to_a;
 			}
 			else if (o_ptr->name2)
 			{
-				if (e_info[o_ptr->name2].flags3 & (TR3_LIGHT_CURSE | TR3_HEAVY_CURSE | TR3_PERMA_CURSE | TR3_UNCONTROLLED))
-					j_ptr->ident |= (IDENT_CURSED);
+				j_ptr->to_h = e_info[o_ptr->name2].max_to_h / 2;
+				j_ptr->to_d = e_info[o_ptr->name2].max_to_d / 2;
+				j_ptr->to_a = e_info[o_ptr->name2].max_to_a / 2;
 			}
 			else
 			{
-				if (k_info[o_ptr->k_idx].flags3 & (TR3_LIGHT_CURSE | TR3_HEAVY_CURSE | TR3_PERMA_CURSE | TR3_UNCONTROLLED))
-					j_ptr->ident |= (IDENT_CURSED);
-				else
-					j_ptr->ident &= ~(IDENT_CURSED | IDENT_BROKEN);
+				j_ptr->to_h = k_info[o_ptr->to_h].to_h;
+				j_ptr->to_d = k_info[o_ptr->to_d].to_d;
+				j_ptr->to_a = k_info[o_ptr->to_a].to_a;
 			}
-
-			/* Hack -- get 'real' value */
-			value = object_value_real(j_ptr);
-
-			/* Apply discount (if any) */
-			if (o_ptr->discount > 0)
+		}
+		if (!object_charges_p(o_ptr)) j_ptr->charges = 0;
+		if (!object_pval_p(o_ptr))
+		{
+			if (o_ptr->name1)
 			{
-				value -= (value * o_ptr->discount / 100L);
+				j_ptr->pval = a_info[o_ptr->name1].pval;
 			}
-
-			/* Done */
-			return(value);
+			else if (o_ptr->name2)
+			{
+				j_ptr->pval = e_info[o_ptr->name2].max_pval / 2;
+			}
+			else
+			{
+				j_ptr->pval = k_info[o_ptr->to_h].pval;
+			}
 		}
 
+		/* Try curse analysis */
+		if (o_ptr->name1)
+		{
+			if (a_info[o_ptr->name1].flags3 & (TR3_LIGHT_CURSE | TR3_HEAVY_CURSE | TR3_PERMA_CURSE | TR3_UNCONTROLLED))
+				j_ptr->ident |= (IDENT_CURSED);
+		}
+		else if (o_ptr->name2)
+		{
+			if (e_info[o_ptr->name2].flags3 & (TR3_LIGHT_CURSE | TR3_HEAVY_CURSE | TR3_PERMA_CURSE | TR3_UNCONTROLLED))
+				j_ptr->ident |= (IDENT_CURSED);
+		}
+		else
+		{
+			if (k_info[o_ptr->k_idx].flags3 & (TR3_LIGHT_CURSE | TR3_HEAVY_CURSE | TR3_PERMA_CURSE | TR3_UNCONTROLLED))
+				j_ptr->ident |= (IDENT_CURSED);
+			else
+				j_ptr->ident &= ~(IDENT_CURSED | IDENT_BROKEN);
+		}
+
+		/* Hack -- get 'real' value */
+		value = object_value_real(j_ptr);
+
+		/* Apply discount (if any) */
+		if (o_ptr->discount > 0)
+		{
+			value -= (value * o_ptr->discount / 100L);
+		}
+	}
+	/* Unknown items -- acquire a base value */
+	else
+	{
 		/* Base value (see above) */
-		else value = object_value_base(o_ptr);
+		value = object_value_base(o_ptr);
 
 		/* Hack -- assess known pval */
 		if (object_pval_p(o_ptr))
